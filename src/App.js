3c4255e11c66319805b002042354206f2225cd40
JUBRIL <jubfeb20@gmail.com>
2023-09-17 11:04:00 +0100

    changes made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-
 import React, { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Homepage from "./HngApi/Homepage";
 import "./App.css";
 import Card from "./HngApi/Card";
 import style from "./Css/card.module.css";
+import styles from "./Css/next.module.css";
 import Pagination from "react-paginate";
 
 function App() {
@@ -14,6 +14,8 @@ function App() {
   const [card, setCard] = useState([]);
   const [currentPage, setCurrentPage] = useState(0); // Track the current page
   const itemsPerPage = 10; // Number of items per page
+
+  const navigate = useNavigate()
 
   const fetchPopular = async () => {
     const url =
@@ -58,6 +60,7 @@ function App() {
     return (
       <Card
         key={res.id}
+        id = {res.id}
         img={`https://image.tmdb.org/t/p/original/${res.poster_path}`}
         release={res.release_date}
         title={res.title}
@@ -82,6 +85,45 @@ function App() {
           onPageChange={handlePageChange}
         />
       </main>
+
+      {/* continue experimenting incase router works */}
+      <div className={styles.nextFlex}>
+        <div className={styles.Flexb}>
+          <h3 className={styles.movie}>
+            <span></span>MovieBox
+          </h3>
+
+          <p className={styles.para}>
+            <span></span>Home
+          </p>
+
+          <p className={styles.paramovs}>
+            <span></span>movies
+          </p>
+
+          <p className={styles.paraseries}>
+            <span></span>TV Series
+          </p>
+
+          <p className={styles.upcoming}>
+            <span></span>Upcoming
+          </p>
+
+          <div className={styles.play_mov}>
+            <p className={styles.mova}>
+              Play movie quizes and earn free tickets
+            </p>
+
+            <p className={styles.paying}>50k people are playing now</p>
+
+            <button className={styles.playbtn}>Start playing</button>
+          </div>
+          <a className={styles.logout} href="">Log out</a>
+        </div>
+
+        {/* Second div */}
+        <div></div>
+      </div>
     </div>
   );
 }
--- src/HngApi/CardDetails.js
+++ src/HngApi/CardDetails.js
@@ -1,36 +1,67 @@
 // import React from "react";
-// // import { useParams, useLocation } from "react-router-dom"; // import useParams and useLocation hooks
-
-// // function CardDetails() {
-// //   const { id } = useParams(); // get the id parameter
-// //   const { state } = useLocation(); // get the state object
-// //   const { title } = state; // get the title from state
-
-// //   // fetch the data for the card using the id
-// //   // ...
-
-// //   return (
-// //     <div>
-// //       {/* display the card details */}
-// //       <h1>{title}</h1>
-// //       <p>{id}</p>
-// //       {/* ... */}
-// //     </div>
-// //   );
-// // }
-// import { useLocation } from "react-router-dom";
+// import { useParams, useLocation } from "react-router-dom"; // import useParams and useLocation hooks
+// import { useParams } from "react-router-dom";
 
 // function CardDetails() {
-//   const location = useLocation();
-//   const { title } = location.state;
+//   const { id } = useParams();
+//   const { title } = window.history.state;
+
+//   // Fetch the data for the card using the id
+//   // ...
 
 //   return (
 //     <div>
-//       <h1>Card Details</h1>
-//       <p>Title: {title}</p>
-//       {/* Add more details or content related to the card */}
+//       {/* Display the card details */}
+//       <h1>{title}</h1>
+//       <p>{id}</p>
+//       <h1>Hello world</h1>
 //     </div>
 //   );
 // }
 
 // export default CardDetails;
+
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"; // Import useParams to get the movie ID from the URL
+
+function CardDetails() {
+  const { id } = useParams(); // Get the movie ID from the URL
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    // Fetch detailed information for the movie using the movie ID
+    const fetchMovieDetail = async () => {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${id}?api_key=af7e4b96fb29cafd40930854b445b800&language=en-US`
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          setMovie(data);
+        } else {
+          throw new Error("Movie not found");
+        }
+      } catch (err) {
+        console.error(err);
+        // Handle errors, e.g., display a message or redirect to an error page
+      }
+    };
+
+    fetchMovieDetail();
+  }, [id]);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>{movie.title}</h2>
+      <p>{movie.overview}</p>
+      {/* Render other movie details as needed */}
+    </div>
+  );
+}
+
+export default CardDetails;
